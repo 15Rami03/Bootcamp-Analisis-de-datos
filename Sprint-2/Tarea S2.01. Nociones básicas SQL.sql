286e6b6501e023f3a8611561e478916791ba77b5
RAMIRO AGUILAR <102828988+15Rami03@users.noreply.github.com>
2025-09-18 17:01:05 +0200

    Subiendo archivos iniciales del Bootcamp

---
+++ Sprint-2/Tarea S2.01. Nociones básicas SQL.sql
@@ -0,0 +1,143 @@
+#NIVEL 1
+#Ejercicio 2
+#Listado de los países que están generando ventas.
+SELECT DISTINCT country 
+FROM company c
+JOIN transaction t
+ON c.id = t.company_id
+WHERE t.amount > 0
+ORDER BY country ASC;
+
+#Desde cuántos países se generan las ventas.
+SELECT COUNT(DISTINCT country) AS cantidad_paises_con_ventas
+FROM company c
+JOIN transaction t
+ON c.id = t.company_id
+WHERE t.amount > 0;
+
+# Identifica la compañía con la mayor media de ventas
+SELECT c.company_name, AVG(t.amount) AS max_average_sale
+FROM company c
+JOIN transaction t 
+  ON c.id = t.company_id
+WHERE t.amount > 0
+GROUP BY c.company_name
+ORDER BY max_average_sale DESC
+LIMIT 1;
+
+#Ejercicio 3
+#Muestra todas las transacciones realizadas por empresas de Alemania.
+SELECT t.id, t.amount,
+       (SELECT c.company_name
+        FROM company c
+        WHERE c.id = t.company_id) AS company_name,
+        (SELECT c.country
+        FROM company c
+        WHERE c.id = t.company_id) AS country
+FROM transaction t
+WHERE t.company_id IN (
+    SELECT id
+    FROM company
+    WHERE country = 'Germany'
+);
+
+#Lista las empresas que han realizado transacciones por un amount superior a la media de todas las transacciones.
+SELECT company_name
+FROM company
+WHERE id IN (
+	SELECT company_id
+	FROM transaction
+	WHERE amount > (SELECT ROUND(AVG(t.amount), 2) FROM transaction t));
+
+
+#Eliminarán del sistema las empresas que carecen de transacciones registradas, entrega el listado de estas empresas.
+SELECT c.company_name
+FROM company c
+WHERE c.id NOT IN (
+				SELECT DISTINCT t.company_id
+				FROM transaction t
+                )
+;
+
+#NIVEL 2
+#Ejercicio 1
+#Identifica los cinco días que se generó la mayor cantidad de ingresos en la empresa por ventas. 
+#Muestra la fecha de cada transacción junto con el total de las ventas.
+
+SELECT DATE(timestamp) AS `date`, SUM(amount) AS total_sales
+FROM transaction
+GROUP BY DATE(timestamp)
+ORDER BY total_sales DESC
+LIMIT 5;
+
+#Ejercicio 2
+#¿Cuál es la media de ventas por país? Presenta los resultados ordenados de mayor a menor medio.
+SELECT c.country, AVG(t.amount) AS avg_sales
+FROM company c
+JOIN transaction t
+ON c.id = t.company_id
+GROUP BY c.country
+ORDER BY AVG(t.amount) DESC;
+
+
+#Ejercicio 3
+#En tu empresa, se plantea un nuevo proyecto para lanzar algunas campañas publicitarias para hacer competencia a la compañía “Non Institute”. 
+#Para ello, te piden la lista de todas las transacciones realizadas por empresas que están ubicadas en el mismo país que esta compañía.
+
+#Muestra el listado aplicando solo subconsultas.
+SELECT *
+FROM (
+    SELECT t.id, 
+           t.amount, 
+           t.timestamp,
+           c.company_name,
+           c.country
+    FROM company c, transaction t
+    WHERE c.id = t.company_id
+) sub
+WHERE sub.country IN (
+    SELECT c.country
+    FROM company c
+    WHERE c.company_name = 'Non Institute'
+);
+
+#Muestra el listado aplicando JOIN y subconsultas.       
+SELECT t.id, t.amount, t.timestamp, c.company_name, c.country
+FROM transaction t 
+JOIN company c
+ON c.id = t.company_id
+WHERE c.country IN (
+	SELECT c2.country
+    FROM company c2
+    WHERE c2.company_name = 'Non Institute');
+    
+#Nivel 3
+#Ejercicio 1
+#Presenta el nombre, teléfono, país, fecha y amount, de aquellas empresas que realizaron transacciones con un valor comprendido entre 350 y 400 euros 
+#y en alguna de estas fechas: 29 de abril de 2015, 20 de julio de 2018 y 13 de marzo de 2024. Ordena los resultados de mayor a menor cantidad.
+
+SELECT c.company_name, c.phone, c.country, DATE(t.timestamp), t.amount
+FROM company c
+JOIN transaction t 
+ON c.id = t.company_id
+WHERE (t.amount BETWEEN 350 AND 400) AND (DATE(t.timestamp) IN ( '2015-04-29', '2018-07-20', '2024-03-13' ))
+ORDER BY t.amount DESC;
+
+#Ejercicio 2
+#Necesitamos optimizar la asignación de los recursos y dependerá de la capacidad operativa que se requiera, 
+#por lo que te piden la información sobre la cantidad de transacciones que realizan las empresas, pero el departamento de recursos humanos es exigente 
+#y quiere un listado de las empresas en las que especifiques si tienen más de 400 transacciones o menos.
+
+SELECT 
+    company_name,
+    (SELECT COUNT(*)
+     FROM transaction t
+     WHERE t.company_id = c.id) AS total_transactions,
+    CASE
+        WHEN (SELECT COUNT(*)
+              FROM transaction t
+              WHERE t.company_id = c.id) > 400 THEN 'More than 400'
+        ELSE '400 or less'
+    END AS transaction_category
+FROM company c
+ORDER BY company_name;
