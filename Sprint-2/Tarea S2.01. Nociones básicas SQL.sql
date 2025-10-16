2f2e657541b82197a7cccf2f75a75399ce206d3a
RAMIRO AGUILAR <102828988+15Rami03@users.noreply.github.com>
2025-10-16 11:17:06 +0200

    Agrego Sprint-5 y cambios en el Sprint-2

--- Sprint-2/Tarea S2.01. Nociones básicas SQL.sql
+++ Sprint-2/Tarea S2.01. Nociones básicas SQL.sql
@@ -11,14 +11,14 @@ ORDER BY country ASC;
 #Desde cuántos países se generan las ventas.
 SELECT COUNT(DISTINCT country) AS cantidad_paises_con_ventas
 FROM company c
-JOIN transaction t
-ON c.id = t.company_id
-WHERE t.amount > 0;
+JOIN transaction
+ON c.id = company_id
+WHERE declined = 0;   -- Aqui tenia amount>0 pero me dijeron que utilizara declined
 
 # Identifica la compañía con la mayor media de ventas
 SELECT company_name, ROUND(AVG(amount), 2) AS max_average_sale
 FROM company c
-JOIN transaction t 
+JOIN transaction
   ON c.id = company_id
 WHERE declined = 0
 GROUP BY company_name
@@ -44,27 +44,27 @@ WHERE company_id IN (
 #Lista las empresas que han realizado transacciones por un amount superior a la media de todas las transacciones.
 SELECT company_name
 FROM company
-WHERE id = ANY (											-- Aqui me pidieorn utilizar any en lugar de in, mas eficiente
+WHERE id = ANY (				-- Aqui me pidieron utilizar any o exist en lugar de in, mas eficiente
 	SELECT company_id
 	FROM transaction
 	WHERE amount > (SELECT AVG(amount) FROM transaction ));
 
 
 #Eliminarán del sistema las empresas que carecen de transacciones registradas, entrega el listado de estas empresas.
-SELECT c.company_name
+SELECT company_name
 FROM company c
-WHERE c.id NOT IN (
-				SELECT DISTINCT t.company_id
-				FROM transaction t
-                )
-;
+WHERE NOT EXISTS (
+    SELECT 1			-- dentro del subquery es una práctica común: no importa qué selecciones, solo interesa si existe al menos una fila.
+    FROM transaction
+    WHERE company_id = c.id
+);
 
 #NIVEL 2
 #Ejercicio 1
 #Identifica los cinco días que se generó la mayor cantidad de ingresos en la empresa por ventas. 
 #Muestra la fecha de cada transacción junto con el total de las ventas.
 
-SELECT DATE(timestamp) AS `date`, SUM(amount) AS total_sales
+SELECT DATE(timestamp) AS date, SUM(amount) AS total_sales
 FROM transaction
 GROUP BY DATE(timestamp)
 ORDER BY total_sales DESC
@@ -72,12 +72,12 @@ LIMIT 5;
 
 #Ejercicio 2
 #¿Cuál es la media de ventas por país? Presenta los resultados ordenados de mayor a menor medio.
-SELECT c.country, AVG(t.amount) AS avg_sales
+SELECT country, ROUND(AVG(amount), 2) AS avg_sales
 FROM company c
-JOIN transaction t
-ON c.id = t.company_id
-GROUP BY c.country
-ORDER BY AVG(t.amount) DESC;
+JOIN transaction
+ON c.id = company_id
+GROUP BY country
+ORDER BY avg_sales DESC;
 
 
 #Ejercicio 3
@@ -85,59 +85,47 @@ ORDER BY AVG(t.amount) DESC;
 #Para ello, te piden la lista de todas las transacciones realizadas por empresas que están ubicadas en el mismo país que esta compañía.
 
 #Muestra el listado aplicando solo subconsultas.
-SELECT *
-FROM (
-    SELECT t.id, 
-           t.amount, 
-           t.timestamp,
-           c.company_name,
-           c.country
-    FROM company c, transaction t
-    WHERE c.id = t.company_id
-) sub
-WHERE sub.country IN (
-    SELECT c.country
-    FROM company c
-    WHERE c.company_name = 'Non Institute'
+SELECT t.id, amount, timestamp, company_name, country
+FROM company c, transaction t
+WHERE c.id = company_id AND c.country IN (
+										SELECT country
+										FROM company c				-- tanto en la consuta inicial como en las subconsultas puedo utilizar el mismo alias para la tabla
+										WHERE company_name = 'Non Institute'
 );
 
 #Muestra el listado aplicando JOIN y subconsultas.       
-SELECT t.id, t.amount, t.timestamp, c.company_name, c.country
+SELECT t.id, amount, timestamp, company_name, country
 FROM transaction t 
 JOIN company c
-ON c.id = t.company_id
-WHERE c.country IN (
-	SELECT c2.country
-    FROM company c2
-    WHERE c2.company_name = 'Non Institute');
+ON c.id = company_id
+WHERE country IN (
+	SELECT country
+    FROM company
+    WHERE company_name = 'Non Institute');
     
 #Nivel 3
 #Ejercicio 1
 #Presenta el nombre, teléfono, país, fecha y amount, de aquellas empresas que realizaron transacciones con un valor comprendido entre 350 y 400 euros 
 #y en alguna de estas fechas: 29 de abril de 2015, 20 de julio de 2018 y 13 de marzo de 2024. Ordena los resultados de mayor a menor cantidad.
 
-SELECT c.company_name, c.phone, c.country, DATE(t.timestamp), t.amount
+SELECT company_name, phone, country, DATE(timestamp), amount
 FROM company c
-JOIN transaction t 
-ON c.id = t.company_id
-WHERE (t.amount BETWEEN 350 AND 400) AND (DATE(t.timestamp) IN ( '2015-04-29', '2018-07-20', '2024-03-13' ))
-ORDER BY t.amount DESC;
+JOIN transaction 
+ON c.id = company_id
+WHERE (amount BETWEEN 350 AND 400) AND (DATE(timestamp) IN ( '2015-04-29', '2018-07-20', '2024-03-13' ))
+ORDER BY amount DESC;
 
 #Ejercicio 2
 #Necesitamos optimizar la asignación de los recursos y dependerá de la capacidad operativa que se requiera, 
 #por lo que te piden la información sobre la cantidad de transacciones que realizan las empresas, pero el departamento de recursos humanos es exigente 
 #y quiere un listado de las empresas en las que especifiques si tienen más de 400 transacciones o menos.
 
-SELECT 
-    company_name,
-    (SELECT COUNT(*)
-     FROM transaction t
-     WHERE t.company_id = c.id) AS total_transactions,
-    CASE
-        WHEN (SELECT COUNT(*)
-              FROM transaction t
-              WHERE t.company_id = c.id) > 400 THEN 'More than 400'
-        ELSE '400 or less'
-    END AS transaction_category
-FROM company c
+SELECT company_name, COUNT(c.id) AS total_transactions,
+CASE
+	WHEN COUNT(c.id) > 400 THEN "More than 400"
+    ELSE "400 or less"
+END AS transaction_category
+FROM transaction
+JOIN company c ON company_id = c.id
+GROUP BY c.id
 ORDER BY company_name;
